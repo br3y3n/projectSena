d8f3343bfafb12837d5315820aad65378c3357c8
brayan23 <bdelgadomunoz@gmail.com>
2024-02-12 16:22:01 -0500

    Agregué h1 pagina campeonatos

--- src/app/campeonatos/page.jsx
+++ src/app/campeonatos/page.jsx
@@ -4,7 +4,8 @@ export default function principal() {
     return (
       <>
             <article>
-                <div> hola principal</div>
+          <div> hola principal</div>
+          <h1>CAMPEONATOS DISPONIBLES CTPI </h1>
       </article>
       </>
     
