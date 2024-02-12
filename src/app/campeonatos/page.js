2c6c8082b9d26a1954658e5ab087bf2e36578f90
ricardo123nac <esteban1525marti@gmail.com>
2024-02-12 18:30:04 -0500

    Crear campeonato, agregar navbar y scroll

---
+++ src/app/campeonatos/page.js
@@ -0,0 +1,94 @@
+import Navbar from "./Navbar";
+
+export default function Campeonatos() {
+  return (
+    <>
+    <div className="w-9/12 absolute right-0">
+lorem ipsum dolor sit amet, consectetur adip
+      lorem ipsum dolor sit amet, consectetur adip
+
+      lorem ipsum dolor sit amet, consectetur adip
+
+      v
+      lorem ipsum dolor sit amet, consectetur adip
+
+      v
+      lorem ipsum dolor sit amet, consectetur adip
+
+      vercel
+      lorem ipsum dolor sit amet, consectetur adip
+      v
+      lorem ipsum dolor sit amet, consectetur adip
+      lorem ipsum dolor sit amet, consectetur adip
+      lorem ipsum dolor sit amet, consectetur adip
+      lorem ipsum dolor sit amet, consectetur adiplorem ipsum dolor sit amet, consectetur adip
+
+      lorem ipsum dolor sit amet, consectetur adip
+      lorem ipsum dolor sit amet, consectetur adip
+
+
+      lorem ipsum dolor sit amet, consectetur adip
+      lorem ipsum dolor sit amet, consectetur adip
+
+      v
+      lorem ipsum dolor sit amet, consectetur adip
+
+      v
+
+      v
+      lorem ipsum dolor sit amet, consectetur adip
+
+      lorem ipsum dolor sit amet, consectetur adip
+      lorem ipsum dolor sit amet, consectetur adip
+
+      lorem ipsum dolor sit amet, consectetur adip
+      lorem ipsum dolor sit amet, consectetur adip
+
+      lorem ipsum dolor sit amet, consectetur adip
+      lorem ipsum dolor sit amet, consectetur adip
+      lorem ipsum dolor sit amet, consectetur adipv
+
+      lorem ipsum dolor sit amet, consectetur adip
+      lorem ipsum dolor sit amet, consectetur adip
+      lorem ipsum dolor sit amet, consectetur adip
+
+      vercellorem ipsum dolor sit amet, consectetur adip
+
+      lorem ipsum dolor sit amet, consectetur adip
+      lorem ipsum dolor sit amet, consectetur adip
+      v
+      lorem ipsum dolor sit amet, consectetur adip
+      v
+
+      vercellorem ipsum dolor sit amet, consectetur adip
+      v
+      v
+
+      volor sit amet, consectetur adip
+
+      v
+      lorem ipsum dolor sit amet, consectetur adip
+
+      v
+      lorem ipsum dolor sit amet, consectetur adip
+
+      vercel
+      lorem ipsum dolor sit amet, consectetur adip
+      v
+      lorem ipsum dolor sit amet, consectetur adip
+      lorem ipsum dolor sit amet, consectetur adip
+      lorem ipsum dolor sit amet, consectetur adip
+      lorem ipsum dolor sit amet, consectetur adiplorem ipsum dolor sit amet, consectetur adip
+
+      lorem ipsum dolor sit amet, consectetur adip
+      v
+    </div>
+      <div className="w-1/4 fixed h-screen left-0">
+        <Navbar></Navbar>
+      </div>
+      
+      
+    </>
+
+  )
+}
