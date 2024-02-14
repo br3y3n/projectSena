ba11d4b0f494d6c095327461affa3b7d65562a31
Greisy88 <136613293+Greisy88@users.noreply.github.com>
2024-02-14 13:22:20 -0500

    sdb

--- src/app/auth/registro/page.jsx
+++ src/app/auth/registro/page.jsx
@@ -1,18 +1,9 @@
 import React from 'react'
 
-function page() {
+ export default function page() {
   return (
     <>
-    <p>Lorem ipsum dolor sit amet consectetur
-         adipisicing elit. Impedit adipisci ab 
-        doloribus eum? Quos expedita, eum nemo
-         minus earum culpa unde tenetur fugit
-          quo odit suscipit dolor. Error, nobis
-           veritatis.</p>
-
-           <h1>wenas wenas</h1>
+    <h1 className='hola'>wenas wenas</h1>
     </>
   )
 }
-
-export default page
