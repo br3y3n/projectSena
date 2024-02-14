bfa429673397d6000ee9e4b553d8ca11bec45546
ricardo123nac <esteban1525marti@gmail.com>
2024-02-14 16:05:44 -0500

    primer diseño

---
+++ src/app/inicioSesion/organizador/crearCampeonato/PaginaCampeonato.jsx
@@ -0,0 +1,20 @@
+export default function PaginaCampeonato() { 
+    return (
+        <>
+            <div class="contendorPrincipalFormulario">
+                <div class="contenedorFormulario">
+                    <p class="letraBlanca">CREA TU CAMPEONATO</p>
+                    <form class="Formulario">
+                        <fieldset class="letraBlanca">General
+                            <legend>
+                                
+                        </legend>
+                        <label >Nombre del Campeonato</label>
+                        </fieldset>
+                    </form>
+              </div>
+            </div>
+        </>
+    )
+
+}
--- src/app/inicioSesion/organizador/crearCampeonato/page.jsx
+++ src/app/inicioSesion/organizador/crearCampeonato/page.jsx
@@ -1,13 +1,23 @@
 import AsideBar from "@/Components/sideBar";
 import RootLayout from "@/app/layout";
+import PaginaCampeonato from "./PaginaCampeonato";
+import "./crearcampeonato.css"
+import Navbar from "./Navbar";
 
 export default function CrearCampeonato() {
   return (
     <RootLayout>
 
-      <AsideBar></AsideBar>
+      <AsideBar>
+        
+      </AsideBar>
 
       <h1>Content page crear campeonatos</h1>
+      <div class="contenedor2">
+        <PaginaCampeonato />
+      </div>
+     
     </RootLayout>
   )
 }
+     
