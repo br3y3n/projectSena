7c8360808c75fc8935a0da11106015954e1d4493
afreyes <afragredo@gmail.com>
2024-02-14 15:31:54 -0500

    agregar section

--- src/app/inicioSesion/organizador/campeonatos/estadisticas/page.jsx
+++ src/app/inicioSesion/organizador/campeonatos/estadisticas/page.jsx
@@ -1,12 +1,21 @@
 import SideCampeonato from '@/Components/sideCampeonato'
 import RootLayout from '@/app/layout'
+import '../../campeonatos/estadisticas/style.css'
 
+const handleClick = () => {
+return <h1>hola</h1>
+}
 
 export default function Estadisticas() {
   return (
     <RootLayout>
         <SideCampeonato></SideCampeonato>
-        <section>Page estadistica</section>
+      <section className='section'>
+        <button className='btnJornada' onClick={handleClick()} >
+          Jornada
+        </button>
+
+     </section>
     </RootLayout>
   )
 }
