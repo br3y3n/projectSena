43bd1e0e69f081905070a48056fd0bbb55a56378
Greisy88 <136613293+Greisy88@users.noreply.github.com>
2024-02-14 15:25:21 -0500

    registro

--- src/app/auth/registro/page.jsx
+++ src/app/auth/registro/page.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React from 'react';
+import '../registro/styles.css';
 
- export default function page() {
+export default function Registro() {
   return (
     <>
-    <h1 className='hola'>wenas wenas</h1>
+      <div className='mayor'>
+        <h1 className='registro'>Registrate en GoSport</h1>
+        
+        <div className='divdatos'>
+        <form action="datos">
+          <label htmlFor="nombre">Nombres :</label>
+          <input type="text" id="nombre" name="nombre" placeholder="Ingrese su nombres" required />
+          
+          <label htmlFor="apellidos">Apellidos :</label>
+          <input type="text" id="nombre" name="nombre" placeholder="Ingrese sus apellidos" required />
+
+          <label htmlFor="telefono">Telefono :</label>
+          <input type="number" id="telefono" name="telefono" placeholder="ej:3163221523" required />
+
+          <label htmlFor="Genero">Genero :</label>
+          <select name="ciudad" id="ciudad">
+                    <option value="selecciona" disabled selected>selecciona</option>
+                    <option value="Femenino" >Femenino</option>
+                    <option value="medellin">Masculino</option>
+                    <option value="Prefiero no decirlo">Prefiero no decirlo</option>
+                </select> 
+          
+          <br /> <br />
+          <label for="">email: </label>
+          <input type="email" name="email" id="email" placeholder="ej:pepito@gmail.com" />
+
+          <label for="">contraseña</label>
+          <input type="password" placeholder="***********" name="password" id="password"/>
+
+          <label for="">Confirmar Contraseña</label>
+          <input type="password" placeholder="***********" name="password" id="password"/>
+            
+        </form>
+        </div>
+      </div>
     </>
-  )
+  );
 }
