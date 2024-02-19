555ca64515e02d387728db6f13c0a39870bbf036
Dreyerg9@gmail.com <miltonyela@gmail.com>
2024-02-19 15:59:57 -0500

    Crearte Planilla Sin Foto

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
 import Link from "next/link";
-
+import PLanilla from "@/Components/planilla/page";
 export default function Home() {
   return (
-   <main>
-    <h1>Landing Page</h1>
-    <Link href={'/inicioSesion'}>Inicio Sesion</Link><br></br>
-    <Link href={'/recuperarPassoword'}>Recuperar Contraeña</Link>
-   </main>
+    <main>
+      {/* <h1>Landing Page</h1>
+      <Link href={'/inicioSesion'}>Inicio Sesion</Link><br></br>
+      <Link href={'/recuperarPassoword'}>Recuperar Contraeña</Link> */}
+      <PLanilla  />
+    </main>
   );
 }
+
