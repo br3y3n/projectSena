fb2e6657b68b3c7ad6042422c6601ba50bb434ee
Luuix02 <Lufvi02@gmail.com>
2024-02-19 18:26:35 -0500

    Integrantes

---
+++ src/Components/Integrantes.jsx
@@ -0,0 +1,58 @@
+import Image from "next/image"
+import './styleIntegrantes.css'
+export default function Integrantes() {
+    return (
+        <>
+            <section className="sectionP">
+                <figure className="figura">
+                    <span className="fig">
+                        <Image src="/image/imageEstadistica/millonarios.png" width={100} height={60} className='imagen' />
+                        <button className="botonVer">Ver integrantes</button>
+                    </span>
+                </figure>
+
+                
+            </section>
+            
+            
+            
+            
+            
+            
+              
+                    
+                    
+          
+               
+               
+                
+                
+             
+                            
+                           
+              
+       
+            
+
+
+       
+                
+                
+
+         
+
+        </>
+    )
+}
+
+
+
+
+
+/*         
+            <div className='contenedorPartido' >
+                <div className='imagen contenedor'>
+                    <Image src="/nacional.png" width={200} height={50} className='imagen' />
+                </div>
+
+            </div> */
