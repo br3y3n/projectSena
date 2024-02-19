58c6563d7a153b9b926fa84e7254c54210be4071
ricardo123nac <esteban1525marti@gmail.com>
2024-02-19 17:47:05 -0500

    AGREGAR CALENDARIO, SELECTORES, Campos

---
+++ src/app/inicioSesion/organizador/crearCampeonato/Calendario.jsx
@@ -0,0 +1,28 @@
+"use client"
+// import React from "react";
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import interactionPlugin from "@fullcalendar/interaction";
+import timeGridPlugin from "@fullcalendar/timegrid";
+export default function Calendarioo() {
+    return (
+        <div class="calendario">
+        <FullCalendar class="DiseñoDeCalendario"
+            plugins={[dayGridPlugin, interactionPlugin, timeGridPlugin]}
+                headerToolbar={{ 
+                left: 'prev,next today',
+                center: 'title',
+                right: 'dayGridMonth, timeGridWeek'
+            }}
+            events={[]}
+            nowIndicator={true}
+            editable={true}
+            droppable={true}
+            selectable={true}
+            selectMirror={true}
+        // dateClick={{}}
+        // drop={ }
+        // eventClick={}
+            /></div>
+    )
+}
--- src/app/inicioSesion/organizador/crearCampeonato/PaginaCampeonato.jsx
+++ src/app/inicioSesion/organizador/crearCampeonato/PaginaCampeonato.jsx
@@ -1,44 +1,70 @@
-export default function PaginaCampeonato() { 
+//  import Select from "./MySelect"
+import Calendarioo from "./Calendario"
+export default function PaginaCampeonato() {
     return (
         <>
-  <div class="contendorPrincipalFormulario">
-          <div class="contenedorCrearCampeonato">
-            <section class="seccionText">  
-            <div class="div">
-            <h1 class="h1">
-                <span class="spanText">CREA TU CAMPEONATO</span>
-                </h1>
-            </div>
-                 </section>
-             <div class="Formulario">
-                 <fieldset class="fieldset">
-                  <legend class="general" >General  </legend>
-                    
-                <div class="form-group"> 
-                   <input className="input" type="text" placeholder="Nombre del campeonato"/> 
-                   <label className="form-label">Ingrese nombre del campeonato</label>
-                    
-                </div>
-                      
-                <div class="form-group"> 
-                   <input className="input" type="text" placeholder="Descripcion del campeonato"/> 
-                   <label className="form-label">Descripcion del campeonato</label>
-                    
-                </div>
-                <div class="selectores">
-                    <p>Discipla</p>
-                <ol>
-                        <li>Futbol</li>
-                        <li>Microfutbol</li>
-                        <li>Futbol Sala</li>
-                        <li></li>
-                    </ol>     
-                    </div>                    
-                        </fieldset>
-               
-               
+            <div class="contendorPrincipalFormulario">
+                <div class="contenedorCrearCampeonato">
+                    <section class="seccionText">
+                        <div class="div">
+                            <h1 class="h1">
+                                <span class="spanText">CREA TU CAMPEONATO</span>
+                            </h1>
                         </div>
-                          </div>
+                    </section>
+                    <div class="Formulario">
+                        <fieldset class="fieldset">
+                            <legend class="legend" >GENERAL </legend>
+                            <div class="form-group">
+                                <input type="text" placeholder="Nombre del campeonato" />
+                                <label className="form-label">Ingrese nombre del campeonato</label>
+                            </div>
+        
+                            <div class="form-group">
+                                <input type="text" placeholder="Descripcion del campeonato" />
+                                <label className="form-label">Descripcion del campeonato</label>
+                            </div>
+                            <div>
+                                {/* <Select />  */}
+                               
+                                <div className="selectores">
+                                    <p>Disciplina</p>
+                                    <select>
+                                        <option class="opcionesSelector" selected disabled hidden value="">Seleccione</option>
+                                        <option>Futbol</option>
+                                        <option>Futbol sala</option>
+                                        <option>Microfutbol</option>
+                                    </select>
+                                </div>
+                                <div className="selectores">
+                                    <p>Categoria</p>
+                                    <select>
+                                    <option class="opcionesSelector" selected disabled hidden value="">Seleccione</option>
+                                    <option>Femenino</option>
+                                    <option>Masculino</option>
+                                    <option>Mixto</option>
+                                    </select>
+                                </div>
+                                <div className="selectoresFinal">
+                                    <p>Modalidad</p>
+                                    <select>
+                                        <option class="opcionesSelector" selected disabled hidden value="">Seleccione</option>
+                                        <option>Interfichas</option>
+                                        <option>Relampago</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </fieldset>
+
+                        <fieldset class="fieldset">
+                            <legend class="legend">FECHAS DE INSCRIPCIONES</legend>
+                           
+                                <Calendarioo />
+                           
+                        </fieldset>
+
+                    </div>
+                </div>
 
             </div>
         </>
