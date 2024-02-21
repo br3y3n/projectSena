6a9ae8218c2eb557bbbfbe3f79e28b22b2054308
MrSenior98 <cfelipecm26@gmail.com>
2024-02-21 15:46:51 -0500

    inicio sesion

--- src/app/inicioSesion/page.jsx
+++ src/app/inicioSesion/page.jsx
@@ -2,25 +2,27 @@ import Link from 'next/link'
 import './style.css'
 export default function InicioSesion() {
   return (
-    <div>
-    
+    <div className='mayor'>
+      <h1>Inicio se sesion</h1>
+      <br /> <br />
 
-      <div className="contenedor-login">
-        <h2>Iniciar Sesión</h2>
-        <form>
-          <div className="campo">
-            <label htmlFor="email">Correo Electrónico:</label>
-            <input type="email" id="email" name="email" required />
-          </div>
-          <div className="campo">
-            <label htmlFor="password">Contraseña:</label>
-            <input type="password" id="password" name="password" required />
-          </div>
-          <button type="submit">Iniciar Sesión</button>
+
+      <div className='datos'>
+        <form action="datos">
+          <label htmlFor="email">correo electronico</label>
+          <input type="email" id="email" name="email" placeholder="ej:pepito@gmail.com" required/>
+
+          <label htmlFor="password">Contraseña</label>
+          <input type="password" id="password" name="password" placeholder="***********" required />
+
+
+          <br />
+          <button>Inicio de sesion</button>
+
+          <h1> ¿No tienes cuenta? <Link href="/auth/registro">Registrate</Link></h1>
         </form>
+
       </div>
-
-
 
     </div>
   )
