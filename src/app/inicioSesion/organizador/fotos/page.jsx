823a578bfbc3a4e566828e3c103732e70a458d13
[brayan23] <[bdelgadomunoz@gmail.com]>
2024-02-24 00:14:58 -0500

    Cree pagina fotos

--- src/app/inicioSesion/organizador/fotos/page.jsx
+++ src/app/inicioSesion/organizador/fotos/page.jsx
@@ -1,13 +1,27 @@
 import AsideBar from "@/Components/sideBar";
 import RootLayout from "@/app/layout";
-
+import Image from "next/image";
+import './style.css'
 export default function Fotos() {
   return (
     <RootLayout>
 
     <AsideBar></AsideBar>
 
-    <h1>Content page fotos</h1>
+    <section className="body">
+        <h1 className="textI">Comparte tus mejores momentos deportivos</h1>
+      <article className="contenedor">
+        <div className="column1">
+            <Image src={'/image/imgFotos/liverpool.png'} width={100} height={100}></Image>
+           <figure className="text">Equipo ganador del campeonato interfichas <p className="textE">ADSO</p></figure> 
+           
+        </div>
+        <div className="column2">
+          <Image src={'/image/iconosSide/fotos.png'} width={50} height={50}></Image>
+        </div>
+      </article>
+      <button className="boton">Publicar</button>
+    </section>
   </RootLayout>
   )
 }
