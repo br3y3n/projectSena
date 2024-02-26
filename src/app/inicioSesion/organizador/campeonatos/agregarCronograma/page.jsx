af1ae8c2c11b1f0166aa4b8a515fda5701b2a847
maryyuu <charigdlopez@gmail.com>
2024-02-26 13:35:24 -0500

    Agregue Cronogrmma

--- src/app/inicioSesion/organizador/campeonatos/agregarCronograma/page.jsx
+++ src/app/inicioSesion/organizador/campeonatos/agregarCronograma/page.jsx
@@ -1,22 +1,47 @@
 import SideCampeonato from '@/Components/sideCampeonato'
 import RootLayout from '@/app/layout'
-
-
+import "./Cronograma.css"
+import ComponenteCronograma from './EditorCronograma'
+// import Image from 'next/image'
 export default function Cronograma() {
   return (
     <RootLayout>
         <SideCampeonato/>
       <div class="ContainerPrincipal">
-        <header class="contenedor Cronograma">
+        <header class="contenedorCronograma">
           <section class="Seccion">
             <select name="" id="">
               <option value="">Jornada Mañana</option>
               <option value="">Jornada Tarde</option>
             </select>
-            <h1>Mañana</h1>
-         </section>
+            </section>
+            <section>
+            <h1 class="texto">MAÑANA</h1>
+            </section>
+         
         </header>
-        <div>
+        
+        <div class="contendorEquipos">
+        <ComponenteCronograma texto2={"Cali"} texto1={"Once Caldas"}
+        imagen1={"./Image/descarga.svg"}
+        imagen2={"/src/app/inicioSesion/organizador/campeonatos/agregarCronograma/Image/nacional.png"}
+        />
+        <ComponenteCronograma texto2={"Nacional"} texto1={"America"}
+        imagen1={"./Image/descarga.svg"}
+        imagen2={"/src/app/inicioSesion/organizador/campeonatos/agregarCronograma/Image/nacional.png"}
+        />
+          <ComponenteCronograma texto2={"Alemania"} texto1={"Francia"}
+        imagen1={"./Image/descarga.svg"}
+        imagen2={"/src/app/inicioSesion/organizador/campeonatos/agregarCronograma/Image/nacional.png"}
+        />
+          <ComponenteCronograma texto2={"Millonarios"} texto1={"Pasto"}
+        imagen1={"./Image/descarga.svg"}
+        imagen2={"/src/app/inicioSesion/organizador/campeonatos/agregarCronograma/Image/nacional.png"}
+        />
+          <ComponenteCronograma texto2={"Colombia"} texto1={"Venezuela"}
+        imagen1={"./Image/descarga.svg"}
+        imagen2={"/src/app/inicioSesion/organizador/campeonatos/agregarCronograma/Image/nacional.png"}
+        />
 
         </div>
         
